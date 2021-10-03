286f685ca154465d2732c47a5dcc9402e7576291
Cristhian-elvis <cristhianvv2@gmail.com>
2021-10-03 08:41:43 -0500

    bd final

--- ocdaApp/admin.py
+++ ocdaApp/admin.py
@@ -1,26 +1,114 @@
 from django.contrib import admin
 
-from ocdaApp.models import Alumno, Area, Curso, Docente, Nota
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+from ocdaApp.models import Alumno, Area, Curso, CursoMatriculado, Docente, Nota
 
 # Register your models here.
 
-class AlumnoAdmin(admin.ModelAdmin):
-    pass
+class AlumnoResource(resources.ModelResource):
+    class Meta:
+        model = Alumno
 
-class DocenteAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Alumno)
+class AlumnoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
 
-class AreaAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'dni',
+        'nombre',
+        'apellido',
+        'promedio'
+    )
 
-class CursoAdmin(admin.ModelAdmin):
-    pass
+    resource_class = AlumnoResource
 
-class NotaAdmin(admin.ModelAdmin):
-    pass
+#---------------
 
-admin.site.register(Alumno, AlumnoAdmin)
-admin.site.register(Docente, DocenteAdmin)
-admin.site.register(Area, AreaAdmin)
-admin.site.register(Curso, CursoAdmin)
-admin.site.register(Nota, NotaAdmin)
+class DocenteResource(resources.ModelResource):
+    class Meta:
+        model = Docente
+
+@admin.register(Docente)
+class DocenteAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+
+    list_display = (
+        'dni',
+        'nombre',
+        'apellido'
+    )
+
+    resource_class = DocenteResource
+
+#---------------
+
+class AreaResource(resources.ModelResource):
+    class Meta:
+        model = Area
+
+@admin.register(Area)
+class AreaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+
+    list_display = (
+        'nombre',
+    )
+
+    resource_class =AreaResource
+
+#---------------
+
+class CursoResource(resources.ModelResource):
+    class Meta:
+        model = Curso
+
+@admin.register(Curso)
+class CursoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+
+    list_display = (
+        'nombre',
+        'docente',
+        'area'
+    )
+
+    resource_class =CursoResource
+
+#---------------
+
+class CursoMatriculadoResource(resources.ModelResource):
+    class Meta:
+        model = CursoMatriculado
+
+@admin.register(CursoMatriculado)
+class CursoMatriculadoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+
+    list_display = (
+        'alumno',
+        'curso',
+        'nota_Final'
+    )
+
+    resource_class =CursoMatriculadoResource
+
+#---------------
+
+class NotaResource(resources.ModelResource):
+    class Meta:
+        model = Nota
+
+@admin.register(Nota)
+class NotaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    
+    list_display = (
+        'cursoMatriculado',
+        'trimestre_1',
+        'trimestre_2',
+        'trimestre_3'
+    )
+
+    resource_class =NotaResource
+
+#admin.site.register(Alumno, AlumnoAdmin)
+#admin.site.register(Docente, DocenteAdmin)
+#admin.site.register(Area, AreaAdmin)
+#admin.site.register(Curso, CursoAdmin)
+#admin.site.register(Nota, NotaAdmin)
--- ocdaApp/models.py
+++ ocdaApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Alumno(models.Model):
-    dni = models.IntegerField(default=0, max_length=8)
+    dni = models.IntegerField(default=0)
     nombre = models.CharField(max_length=15)
     apellido = models.CharField(max_length=15)
     promedio = models.IntegerField(default=0)
@@ -11,16 +11,17 @@ class Alumno(models.Model):
     def __str__(self):
         return self.nombre
 
-
+#----------------
 
 class Docente(models.Model):
-    dni = models.IntegerField(default=0, max_length=8)
+    dni = models.IntegerField(default=0)
     nombre = models.CharField(max_length=15)
     apellido = models.CharField(max_length=15)
 
     def __str__(self):
         return self.nombre
 
+#----------------
 
 class Area(models.Model):
     nombre = models.CharField(max_length=50)
@@ -28,26 +29,35 @@ class Area(models.Model):
     def __str__(self):
         return self.nombre
 
-
-
+#----------------
 
 class Curso(models.Model):
-    nombre = models.CharField(max_length=15)
+    nombre = models.CharField(max_length=50)
     
-    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    #alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
     docente = models.ForeignKey(Docente, on_delete=models.CASCADE)
     area = models.ForeignKey(Area, on_delete=models.CASCADE)
 
-    nota_Final = models.IntegerField(default=0)
+    #nota_Final = models.IntegerField(default=0)
 
     def __str__(self):
         return str(self.nombre)
 
+#----------------
 
+class CursoMatriculado(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
 
+    nota_Final = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.alumno) + " " + str(self.curso)
+
+#----------------
 
 class Nota(models.Model):
-    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    cursoMatriculado = models.ForeignKey(CursoMatriculado, on_delete=models.CASCADE)
 
     trimestre_1 = models.IntegerField(default=0)
     trimestre_2 = models.IntegerField(default=0)
